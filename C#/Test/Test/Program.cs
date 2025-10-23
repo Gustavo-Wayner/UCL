a7717db03040d081bb6efbceef06a6a48f96ed77
Gustavo-Wayner <gustavowayner@ucl.br>
2025-10-23 09:51:40 -0300

    Adicionados dois projetos .NET
    Projeto de teste e atividade que o professor pediu para fazer antes que eu conseguisse dar commit em Test

---
+++ C#/Test/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test;
+
+public static class Program
+{
+    public static void Main()
+    {
+        List<int> vec = new List<int> { 1, 3, 5, 7 };
+        vec.Add(9);
+
+        for(int i = 0; i < vec.Count; i++)
+        {
+            Console.WriteLine($"item {i} da lista: {vec[i]}");
+        }
+    }
+}
---
+++ C#/VecEven/VecEven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VecEven;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+
+        int[] vec = new int[3] {1, 2, 3};
+
+        Console.WriteLine("Esses são os elementos pares do vetor: ");
+        for (int i = 0; i < vec.Length; i++)
+        {
+            if (i % 2 == 0) Console.WriteLine(vec[i]);
+        }
+    }
+}
